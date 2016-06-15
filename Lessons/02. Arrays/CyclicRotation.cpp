9a9a182839603f49e495125432a800fdb6c4697b
Sanghyeon Jeon <skyclad0x7b7@gmail.com>
2016-06-15 22:23:41 +0900

    CyclicRotation

---
+++ Lessons/02. Arrays/CyclicRotation.cpp
@@ -0,0 +1,10 @@
+vector<int> solution(vector<int> &A, int K) {
+    vector<int> ret;
+    int len = A.size();
+    if(len == 0) return ret;
+    int c = K % len;
+    for(int i=0; i < len; i++) {
+        ret.push_back(A[(len - (c - i)) % len]);
+    }
+    return ret;
+}
