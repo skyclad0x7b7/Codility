eb430a411fbba040182b1ca2cbba0d3b55445352
Sanghyeon Jeon <skyclad0x7b7@gmail.com>
2016-06-20 23:06:54 +0900

    MinAvgTwoSlice
    Need to patch

--- Lessons/05. Prefix Sums/MinAvgTwoSlice.cpp
+++ Lessons/05. Prefix Sums/MinAvgTwoSlice.cpp
@@ -1,23 +1,22 @@
 int solution(vector<int> &A) {
     int minAvg = 2147483647;
-    int curCount = 2;
+    int curCount;
     int retIndex = 0;
-
-    int sum = 0;
+    double sum = 0;
     double avg = 0;
-    do {
-        for(int i=0; i+curCount < A.size(); i++) {
+
+    for(curCount = 2; curCount < 4; curCount++) {
+        for(int i=0; i<A.size()-(curCount-1); i++) {
+            sum = 0;
             for(int j=0; j<curCount; j++) {
-                sum += A[i+j];
+                sum += A[i + j];
             }
-            avg = (double)sum / curCount;
+            avg = sum / (double)curCount;
             if(avg < minAvg) {
                 minAvg = avg;
                 retIndex = i;
             }
-            sum = 0; 
         }
-        curCount += 1;
-    } while(curCount < A.size());
+    }
     return retIndex;
 }
